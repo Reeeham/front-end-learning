ea45b79c210ce4e1d810310ed42b1193eb16ac7a
@reeeham <reeehamgamal@gmail.com>
2021-05-19 14:02:41 +0200

    tailwindcss-twitter

---
+++ tailwind-twitter/tailwind.config.js
@@ -0,0 +1,26 @@
+module.exports = {
+  purge: [],
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    extend: {
+      colors: {
+      teal:'#47acb4',
+      'grey-darkest': '#3d4852',
+      'grey-darker': '#606f7b',
+      'grey-dark': '#8795a1',
+      'grey': '#b8c2cc',
+      'grey-light': '#dae1e7',
+      'grey-lighter': '#f1f5f8',
+      'grey-lightest': '#f8fafc',
+      'blue':'#00BFFF'
+    },
+    spacing : {
+      112: '28rem',
+      20:'5rem'
+    }},
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+}
